2988e3cd9e083a1d8e79d53b2404340df03ff129
Chasen-Zhang <787025321@qq.com>
2022-04-06 20:43:59 +0800

    fix: hase router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,12 @@
 import { Suspense } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import 'antd/dist/antd.min.css'
 import { AppRouters } from './router';
 import './App.css';
 
 function App() {
-  const { VITE_APP_BASEN_NAME } = import.meta.env;
   return (
-    <Router basename={VITE_APP_BASEN_NAME as string}>
+    <Router>
       <Suspense fallback={<><div></div></>}>
         <AppRouters />
       </Suspense>
