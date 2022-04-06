fd452ef108a104a5d3feb2e0cd748fdc4b244316
Chasen-Zhang <787025321@qq.com>
2022-04-06 14:55:52 +0800

    feat: add date range filter

--- front/src/pages/graphs.tsx
+++ front/src/pages/graphs.tsx
@@ -1,16 +1,24 @@
-import { FC, ReactElement, useState } from 'react';
+import { FC, ReactElement, useState, useEffect } from 'react';
 import { useMount, useUnmount } from 'ahooks';
-import { DatePicker, Form,Row, Col, Select, Button } from 'antd';
+import { DatePicker, Form, Row, Col, Select, Button, message } from 'antd';
 import { getApiListByCategory, getCategories, getGraph } from '../api';
 import { Link } from "react-router-dom";
 import * as echarts from 'echarts';
+import moment from 'moment';
 const { RangePicker } = DatePicker;
 const { Option } = Select;
 let chartInstance:any = {};
 const Graphs: FC = (): ReactElement=> {
   const [category, setCategory] = useState([]);
-  const [defaultCategory, setDefaultCategory] = useState('');
+  const [defaultCategory, setDefaultCategory] = useState<any>('');
   const [formRef] = Form.useForm();
+  const [xAxisdate, setXAxisdate] = useState([]);
+  const [isFullDate, setIsFullDate] = useState(true);
+  const [filterDateObj, setFilterDate] = useState({
+    filterDate: [],
+    startIndex: 0,
+    endIndex: -1
+  });
   const [graphKind] = useState([
     {
       name: 'Raw',
@@ -24,6 +32,16 @@ const Graphs: FC = (): ReactElement=> {
   useUnmount(()=>{
     chartInstance = {};
   });
+  useEffect(()=>{
+    if (!isFullDate) {
+      getDataByFilter();
+    }
+  }, [isFullDate])
+  async function getDataByFilter() {
+    const defaultCategoryList = await getApiListByCategory(defaultCategory);
+    setContainer(defaultCategoryList);
+    getAllGraph(defaultCategory, defaultCategoryList, false);
+  }
   async function getAllInfo(){
     let allCategory = await getCategories();
     setCategory(allCategory || []);
@@ -31,39 +49,28 @@ const Graphs: FC = (): ReactElement=> {
       formRef.setFieldsValue({
         category: allCategory[0]
       });
+      setIsFullDate(true)
       setDefaultCategory(allCategory[0])
       const defaultCategoryList = await getApiListByCategory(allCategory[0]);
-      setContainer(defaultCategoryList)
-      getAllGraph(allCategory[0], defaultCategoryList);
+      setContainer(defaultCategoryList);
+      getAllGraph(allCategory[0], defaultCategoryList, true);
     }
   }
-  async function changeCategory(value: string) {
-    setDefaultCategory(value)
-    const defaultCategoryList = await getApiListByCategory(value);
-    setContainer(defaultCategoryList)
-    getAllGraph(value, defaultCategoryList);
-  }
-  async function  getAllGraph(category:string, graphList:string) {
+  async function  getAllGraph(category:string, graphList:string, isFullDate: boolean) {
     const len = graphList.length;
     for (let i = 0; i < len; i++) {
       const graph = graphList[i];
       const graphData =  await getGraph(category, graph)
-      drawCharts(category, graph, graphData, i);
+      i===0 && setXAxisdate(graphData?.xAxis);
+      drawCharts(category, graph, graphData, i, isFullDate);
+      
     }
   }
-  function drawCharts(category: string,graph: string, graphData: any, i:number) {
+  function drawCharts(category: string,graph: string, graphData: any, i:number, isFullDate: boolean) {
     const name = `${category}-${graph}`;
     const container = document.getElementById(`${name}`) as HTMLElement;
-    let chart = null;
-    if (chartInstance[name]) {
-      chart = chartInstance[name];
-      console.log(1)
-    } else {
-      chart = echarts.init(container);
-      chartInstance[name] = chart;
-      console.log(2)
-    }
-    const {
+    let chart:any = echarts.init(container);
+    let {
       legend,
       lines,
       sql,
@@ -71,9 +78,26 @@ const Graphs: FC = (): ReactElement=> {
       version,
       xAxis
     } = graphData;
+    if (!isFullDate){
+      const { startIndex, endIndex, filterDate } = filterDateObj;
+      if (filterDateObj.endIndex ===-1){
+        xAxis = filterDate;
+        lines.map((item:any)=>{
+          item.data = item.data.slice(startIndex);
+        });
+        version = version.slice(startIndex);
+      } else {
+        xAxis = filterDate;
+        lines.map((item:any)=>{
+          item.data = item.data.slice(startIndex, endIndex+1);
+        });
+        version = version.slice(startIndex, endIndex+1);
+      }
+    }
     lines?.map((item:any)=>{
       return item.type = 'line'
     })
+    setIsFullDate(true)
     chart.setOption({
       legend: {
         data: legend,
@@ -111,6 +135,44 @@ const Graphs: FC = (): ReactElement=> {
       },
       series: lines
     });
+    chart = null;
+  }
+  function disabledRangeTime(current:any) {
+    return current > moment().add(0, 'days');
+ 
+  }
+  async function submit(){
+    const {category, date, kind} = formRef.getFieldsValue();
+    setDefaultCategory(category)
+    let starDate = null;
+    let endDate = null;
+    if (date && date.length>1) {
+      starDate = moment(date[0]).format('yyyy-MM-DD');
+      endDate = moment(date[1]).format('yyyy-MM-DD');
+    }
+   
+    const start = xAxisdate.indexOf(starDate as never);
+    const end =  xAxisdate.indexOf(endDate as never);
+
+    const startIndex = start ===-1 ? 0 : start;
+    const endIndex = end;
+    let realDate = [];
+    if (endIndex===-1){
+      realDate = xAxisdate.slice(startIndex)
+    } else {
+      realDate = xAxisdate.slice(startIndex, endIndex+1)
+    }
+    if (realDate.length<=0) {
+      message.warning('There is no birth data for this time range');
+      return
+    }
+    setIsFullDate(false)
+    console.log('realDate:', xAxisdate, startIndex, endIndex, realDate)
+    setFilterDate({
+      filterDate: realDate,
+      startIndex,
+      endIndex: endIndex
+    });
   }
   return ( 
     <div>
@@ -128,7 +190,9 @@ const Graphs: FC = (): ReactElement=> {
             <Form.Item
               name="date"
               label="Date range">
-              <RangePicker style={{width: '100%'}}/>
+              <RangePicker
+                disabledDate={disabledRangeTime}
+                style={{width: '100%'}}/>
             </Form.Item>
           </Col>
           <Col span={4}>
@@ -146,14 +210,14 @@ const Graphs: FC = (): ReactElement=> {
             <Form.Item
               name="category"
               label="Category">
-              <Select onChange={changeCategory}>
+              <Select>
                 {category.map((item, index)=>{
                   return  <Option key={index} value={item}>{item}</Option>
                 })}
               </Select>
             </Form.Item>
           </Col>
-          <Button type='primary'>Submit</Button>
+          <Button type='primary' onClick={submit}>Submit</Button>
         </Row>
       </Form>
       <div>
@@ -166,7 +230,7 @@ const Graphs: FC = (): ReactElement=> {
               container?.map((item)=>{
                 return <Col span={8}  key={item} style={{marginBottom: '20px'}}>
                   <div style={{height: '300px', width: '100%'}} id={`${defaultCategory}-${item}`}>
-                    {item}
+                   
                   </div>
                 </Col>
               })
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
