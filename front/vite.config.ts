bf860be0bab993d3c27a66fa6039541b8f1beeaf
Chasen-Zhang <787025321@qq.com>
2022-04-06 19:53:33 +0800

    fix: file dir

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'databend-perf',
+  base: './',
   plugins: [react()],
   resolve: {
 		alias: {
