351fdf15222698a54c0d51ddb1fddef9eea27c72
Chasen-Zhang <787025321@qq.com>
2022-04-16 14:58:42 +0800

    feat: add share functions

---
+++ front/src/componnent/ShareButton/index.tsx
@@ -0,0 +1,36 @@
+// Copyright 2022 Datafuse Labs.
+import { Button, notification } from 'antd';
+import { FC, ReactElement } from 'react';
+import IconFont from '../../assets/icon/icon';
+import { copyToClipboard } from '../../utils/tools';
+import styles from './index.module.scss';
+interface IProps {
+  category: string;
+  graph: string;
+  type?: string;
+  title?: string;
+}
+const ShareButton: FC<IProps> = ({category, graph, type, title}): ReactElement=> {
+  // @ts-ignore
+  const { VITE_APP_SHARE_URL } = import.meta.env;
+  const share = ()=>{
+    copyToClipboard(`${VITE_APP_SHARE_URL}?category=${category}&graph=${graph}&type=${type}&title=${title}`);
+    notification.open({
+      message: 'Tips',
+      duration: 1.5,
+      placement: 'topLeft',
+      description:
+        'The share link is already in the clipboard'
+    });
+  }
+  return (
+    <Button onClick={share} title='Share with others' type='default' className={styles.share}>
+      <IconFont className={styles.icon} type='databend-fenxiang'></IconFont>
+    </Button>
+  );
+};
+ShareButton.defaultProps = {
+  type: 'line',
+  title: '',
+}
+export default ShareButton;
--- front/src/pages/graphs.tsx
+++ front/src/pages/graphs.tsx
@@ -8,6 +8,7 @@ import moment from 'moment';
 import { formatterDate } from '../utils/tools';
 import styles from './css/styles.module.scss';
 import { deviceType } from '../utils/device-type';
+import ShareButton from '../componnent/ShareButton';
 const { RangePicker } = DatePicker;
 const { Option } = Select;
 const Graphs: FC = (): ReactElement=> {
@@ -120,8 +121,18 @@ const Graphs: FC = (): ReactElement=> {
       chart.setOption({
         legend: {
           data: legend,
-          y: 'bottom'
+          y: '10'
         },
+        dataZoom: [
+          {
+            type: 'inside',
+          },
+          {
+            type: 'slider',
+            handleIcon:
+            'path://M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z'
+          }
+        ],
         tooltip: {
           extraCssText: 'z-index: 10',
           trigger: 'axis',
@@ -256,7 +267,8 @@ const Graphs: FC = (): ReactElement=> {
                 return <Col span={isPhone?24:8}  key={item} style={{marginBottom: '20px'}}>
                         <div className={styles.content}>
                           <div className={styles.title} id={`${defaultCategory}-${item}-title`}></div>
-                          <div style={{height: '300px', width: '100%', background: '#fff'}} id={`${defaultCategory}-${item}`}></div>
+                          <div style={{height: '340px', width: '100%', background: '#fff'}} id={`${defaultCategory}-${item}`}></div>
+                          <ShareButton category={defaultCategory} graph={item}></ShareButton>
                         </div>
                       </Col>
               })
