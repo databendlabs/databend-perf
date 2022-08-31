0b858407aec62a83bd9f864a660450aa054f0f89
Li Yazhou <me.ssword@gmail.com>
2022-08-31 15:16:22 +0800

    split supplier

--- reload/tpch/supplier.sh
+++ reload/tpch/supplier.sh
@@ -13,8 +13,8 @@ echo "CREATE TABLE IF NOT EXISTS supplier (
 )" | bendctl query --warehouse=$WAREHOUSE
 
 echo "
-COPY INTO region FROM 's3://repo.databend.rs/tpch100/supplier.tbl'
-credentials=(aws_key_id='$AWS_KEY_ID' aws_secret_key='$AWS_SECRET_KEY')
+COPY INTO lineitem FROM 's3://repo.databend.rs/tpch100/supplier/'
+credentials=(aws_key_id='$AWS_KEY_ID' aws_secret_key='$AWS_SECRET_KEY') pattern ='supplier.tbl.*'
 file_format=(type='CSV' field_delimiter='|' record_delimiter='\\n' skip_header=1);
 " | bendctl query --warehouse=$WAREHOUSE
 
