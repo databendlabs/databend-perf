2e3d4de3bb8b09d2e7ec879d9af1b4134ebf677f
Li Yazhou <me.ssword@gmail.com>
2022-08-31 15:46:36 +0800

    add reload partsupp

--- reload/tpch/part.sh
+++ reload/tpch/part.sh
@@ -1,3 +1,7 @@
+echo "
+DROP TABLE IF EXISTS part;
+" | bendctl query --warehouse=$WAREHOUSE
+
 echo "CREATE TABLE IF NOT EXISTS part (
     p_partkey     BIGINT not null,
     p_name        STRING not null,
@@ -9,3 +13,13 @@ echo "CREATE TABLE IF NOT EXISTS part (
     p_retailprice DOUBLE not null,
     p_comment     STRING not null
 )" | bendctl query --warehouse=$WAREHOUSE
+
+echo "
+COPY INTO part FROM 's3://repo.databend.rs/tpch100/part/'
+credentials=(aws_key_id='$AWS_KEY_ID' aws_secret_key='$AWS_SECRET_KEY') pattern ='part.tbl.*'
+file_format=(type='CSV' field_delimiter='|' record_delimiter='\\n' skip_header=1);
+" | bendctl query --verbose --warehouse=$WAREHOUSE
+
+echo "
+SELECT count(*) FROM part;
+" | bendctl query --warehouse=$WAREHOUSE
