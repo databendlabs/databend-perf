ea6318ecb1435cf2980312eb40f94f5b9df2a868
Li Yazhou <me.ssword@gmail.com>
2022-08-31 15:38:05 +0800

    reload customer

--- reload/tpch/customer.sh
+++ reload/tpch/customer.sh
@@ -1,3 +1,7 @@
+echo "
+DROP TABLE IF EXISTS customer;
+" | bendctl query --warehouse=$WAREHOUSE
+
 echo "CREATE TABLE IF NOT EXISTS customer (
     c_custkey     BIGINT not null,
     c_name        STRING not null,
@@ -8,3 +12,13 @@ echo "CREATE TABLE IF NOT EXISTS customer (
     c_mktsegment  STRING not null,
     c_comment     STRING not null
 )" | bendctl query --warehouse=$WAREHOUSE
+
+echo "
+COPY INTO customer FROM 's3://repo.databend.rs/tpch100/customer/'
+credentials=(aws_key_id='$AWS_KEY_ID' aws_secret_key='$AWS_SECRET_KEY') pattern ='customer.tbl.*'
+file_format=(type='CSV' field_delimiter='|' record_delimiter='\\n' skip_header=1);
+" | bendctl query --verbose --warehouse=$WAREHOUSE
+
+echo "
+SELECT count(*) FROM customer;
+" | bendctl query --warehouse=$WAREHOUSE
