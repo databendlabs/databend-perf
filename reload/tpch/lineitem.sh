36f4f7b3604d87f6e26aecbcf1a04f60a67c5dc0
Li Yazhou <me.ssword@gmail.com>
2022-08-31 16:06:33 +0800

    add count

--- reload/tpch/lineitem.sh
+++ reload/tpch/lineitem.sh
@@ -27,4 +27,9 @@ echo "
 COPY INTO lineitem FROM 's3://repo.databend.rs/tpch100/lineitem'
 credentials=(aws_key_id='$AWS_KEY_ID' aws_secret_key='$AWS_SECRET_KEY') pattern ='lineitem.tbl.*'
 file_format=(type='CSV' field_delimiter='|' record_delimiter='\\n' skip_header=1);
+" | bendctl query --verbose --warehouse=$WAREHOUSE
+
+echo "
+SELECT count(*) FROM lineitem;
 " | bendctl query --warehouse=$WAREHOUSE
+
