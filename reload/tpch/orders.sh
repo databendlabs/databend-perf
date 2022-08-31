c1a08c356d2f50836d2878d418bd8868e705c6d7
Li Yazhou <me.ssword@gmail.com>
2022-08-31 15:51:16 +0800

    add reload orders

--- reload/tpch/orders.sh
+++ reload/tpch/orders.sh
@@ -1,5 +1,5 @@
 echo "
-DROP TABLE orders;
+DROP TABLE IF EXISTS orders;
 )" | bendctl query --warehouse=$WAREHOUSE
 
 echo "
@@ -14,3 +14,13 @@ CREATE TABLE IF NOT EXISTS orders (
     o_shippriority   INTEGER not null,
     o_comment        STRING not null
 )" | bendctl query --warehouse=$WAREHOUSE
+
+echo "
+COPY INTO orders FROM 's3://repo.databend.rs/tpch100/orders.tbl'
+credentials=(aws_key_id='$AWS_KEY_ID' aws_secret_key='$AWS_SECRET_KEY')
+file_format=(type='CSV' field_delimiter='|' record_delimiter='\\n' skip_header=1);
+" | bendctl query --warehouse=$WAREHOUSE
+
+echo "
+SELECT count(*) FROM orders;
+" | bendctl query --warehouse=$WAREHOUSE
