fd1a9a3b1f48d397b1c4dfab15cd698f790e99d2
Li Yazhou <me.ssword@gmail.com>
2022-08-31 15:32:45 +0800

    fix typo

--- reload/tpch/supplier.sh
+++ reload/tpch/supplier.sh
@@ -13,10 +13,10 @@ echo "CREATE TABLE IF NOT EXISTS supplier (
 )" | bendctl query --warehouse=$WAREHOUSE
 
 echo "
-COPY INTO lineitem FROM 's3://repo.databend.rs/tpch100/supplier/'
+COPY INTO supplier FROM 's3://repo.databend.rs/tpch100/supplier/'
 credentials=(aws_key_id='$AWS_KEY_ID' aws_secret_key='$AWS_SECRET_KEY') pattern ='supplier.tbl.*'
 file_format=(type='CSV' field_delimiter='|' record_delimiter='\\n' skip_header=1);
-" | bendctl query --warehouse=$WAREHOUSE
+" | bendctl query --verbose --warehouse=$WAREHOUSE
 
 echo "
 SELECT count(*) FROM supplier;
