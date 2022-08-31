61b33c394a529e7f8139e6f24f172cdab0130868
Li Yazhou <me.ssword@gmail.com>
2022-08-31 14:58:12 +0800

    add region reload

--- reload/tpch/region.sh
+++ reload/tpch/region.sh
@@ -1,6 +1,19 @@
+echo "
+DROP TABLE IF EXISTS region;
+" | bendctl query --warehouse=$WAREHOUSE
+
 echo "CREATE TABLE IF NOT EXISTS region (
     r_regionkey  INTEGER not null,
     r_name       STRING not null,
     r_comment    STRING
 )" | bendctl query --warehouse=$WAREHOUSE
 
+echo "
+COPY INTO region FROM 's3://repo.databend.rs/tpch100/region.tbl'
+credentials=(aws_key_id='$AWS_KEY_ID' aws_secret_key='$AWS_SECRET_KEY')
+file_format=(type='CSV' field_delimiter='|' record_delimiter='\\n' skip_header=1);
+" | bendctl query --warehouse=$WAREHOUSE
+
+echo "
+SELECT count(*) FROM region;
+" | bendctl query --warehouse=$WAREHOUSE
