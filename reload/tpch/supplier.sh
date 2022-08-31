99bc346b64cef8bf90f024b620ed714473a8bd4b
Li Yazhou <me.ssword@gmail.com>
2022-08-31 15:01:00 +0800

    add supplier

--- reload/tpch/supplier.sh
+++ reload/tpch/supplier.sh
@@ -1,3 +1,7 @@
+echo "
+DROP TABLE IF EXISTS supplier;
+" | bendctl query --warehouse=$WAREHOUSE
+
 echo "CREATE TABLE IF NOT EXISTS supplier (
     s_suppkey     BIGINT not null,
     s_name        STRING not null,
@@ -7,3 +11,13 @@ echo "CREATE TABLE IF NOT EXISTS supplier (
     s_acctbal     DOUBLE not null,
     s_comment     STRING not null
 )" | bendctl query --warehouse=$WAREHOUSE
+
+echo "
+COPY INTO region FROM 's3://repo.databend.rs/tpch100/supplier.tbl'
+credentials=(aws_key_id='$AWS_KEY_ID' aws_secret_key='$AWS_SECRET_KEY')
+file_format=(type='CSV' field_delimiter='|' record_delimiter='\\n' skip_header=1);
+" | bendctl query --warehouse=$WAREHOUSE
+
+echo "
+SELECT count(*) FROM supplier;
+" | bendctl query --warehouse=$WAREHOUSE
